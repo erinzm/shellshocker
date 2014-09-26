7cb757640e69f0e08012b273f8fc7487e7c5b56c
Liam M <archimedespi3141@gmail.com>
2014-09-26 13:56:33 -0500

    More work on the site.

---
+++ shellshocker_server/static/js/site.js
@@ -0,0 +1,3 @@
+$('.shock-it').on('click', function() {
+  $(this).closest('form').submit();
+});
--- shellshocker_server/views.py
+++ shellshocker_server/views.py
@@ -1,6 +1,15 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from shellshocker_server import app
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
   return  render_template('index.html')
+
+@app.route('/shockit/', methods=['POST'])
+def shockit():
+  websiteUrl = request.form.get('websiteUrl')
+  if request.form.get('commonVulnerableRoutes') is None:
+    commonVulnerableRoutes = False
+  elif request.form.get('commonVulnerableRoutes') == "on":
+    commonVulnerableRoutes = True
+  return 'URL is: ' + websiteUrl + ' commonVulnerableRoutes is: ' + str(commonVulnerableRoutes)
