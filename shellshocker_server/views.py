495f8fe49935ac88ef5c2f94dedff916a7062e2c
Liam M <archimedespi3141@gmail.com>
2014-09-26 17:28:28 -0500

    Add views

--- shellshocker_server/views.py
+++ shellshocker_server/views.py
@@ -1,15 +1,31 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
+from flask import render_template, flash, jsonify, redirect, session, url_for, request, g
 from shellshocker_server import app
+from shellshocker.exploits import ShellShocker
 
 @app.route('/', methods=['GET'])
 def index():
-  return  render_template('index.html')
+  return render_template('index.html')
 
-@app.route('/shockit/', methods=['POST'])
+@app.route('/shockit/', methods=['GET', 'POST'])
 def shockit():
   websiteUrl = request.form.get('websiteUrl')
+  urlsToCheck = [websiteUrl]
   if request.form.get('commonVulnerableRoutes') is None:
     commonVulnerableRoutes = False
   elif request.form.get('commonVulnerableRoutes') == "on":
     commonVulnerableRoutes = True
-  return 'URL is: ' + websiteUrl + ' commonVulnerableRoutes is: ' + str(commonVulnerableRoutes)
+  if commonVulnerableRoutes == True:
+    urlsToCheck.extend(ShellShocker.commonVulnerableRoutes)
+  return render_template('shockit.html',
+    websiteUrl = websiteUrl,
+    commonVulnerableRoutes = commonVulnerableRoutes,
+    urlsToCheck = str(urlsToCheck)
+    )
+
+@app.route('/exploitable', methods=['POST'])
+def exploitable():
+  print 'called route'
+  websiteUrl = request.form.get('websiteUrl')
+  shocker = ShellShocker({'url': websiteUrl})
+  exploitable = shocker.exploitable()
+  return jsonify(exploitable=exploitable)
