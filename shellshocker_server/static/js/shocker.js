162a6d6603d4b4704a0bc7eb266c1b55337cf1ea
Liam M <archimedespi3141@gmail.com>
2014-09-27 14:10:31 -0500

    Comment it, change it so that it works properly on errors

--- shellshocker_server/static/js/shocker.js
+++ shellshocker_server/static/js/shocker.js
@@ -1,11 +1,19 @@
+// When the document is ready
 $(document).ready(function() {
+  // Grab the Handlebars template from a <script> tag
   alertTemplateText = $('#alertTemplateText').html();
+  // Compile the Handlebars template
   alertTemplate = Handlebars.compile(alertTemplateText);
 
+  // For every index of urlsToCheck
   for (var i = 0; i < urlsToCheck.length; i++){
+    // Get the element with that index
     url = urlsToCheck[i]
+    // For every index of headersToCheck
     for (var j = 0; j < headersToCheck.length; j++) {
+      // Get the element with that index
       header = headersToCheck[j]
+      // Define an empty response
       var response = "";
       //console.log('Checking URL ' + url + ' with header ' + header);
       $.ajax({
@@ -17,16 +25,19 @@ $(document).ready(function() {
           header: header
         },
         success: function(result) {
-          response = result;
+          // Is it exploitable?
+          exploitable = result['exploitable'];
+          //console.log("This URL is exploitable? " + exploitable);
+          // Generate the HTML of the template
+          template_html = alertTemplate({exploitable: exploitable, url: url, header: header});
+          // Append it to the results well
+          $('.results-well').append(template_html).fadeIn();
+          //console.log(template_html);
         }
       });
-      exploitable = response['exploitable'];
-      //console.log("This URL is exploitable? " + exploitable);
-      template_html = alertTemplate({exploitable: exploitable, url: url, header: header});
-      $('.results-well').append(template_html).fadeIn();
-      //console.log(template_html);
     }
   }
 
+  // It's all done, hide the spinner ;)
   $('.testing-spinner').hide();
 });
