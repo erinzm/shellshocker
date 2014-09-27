5b5975c70327defba016b000616a84db53141ea3
Liam M <archimedespi3141@gmail.com>
2014-09-27 18:56:16 -0500

    Stop spinner when done :fistpump:

--- shellshocker_server/static/js/shocker.js
+++ shellshocker_server/static/js/shocker.js
@@ -13,22 +13,29 @@ $(document).ready(function() {
     }),
     true);
 
-  console.log(exploits);
+  //console.log(exploits);
 
-  exploits.reduce(function (sequence, exploit) {
-    return sequence.then(function () {
-      console.log(exploit);
-      return post(exploitableCheckURL, {websiteUrl: exploit.url, header: exploit.header});
-    }).then(function(result) {
-      console.log(result);
-      // Generate the HTML of the template
-      template_html = alertTemplate({exploitable: result.response.exploitable, url: result.dat.websiteUrl, header: result.dat.header});
-      // Append it to the results well
-      $('.results-well').append(template_html).fadeIn();
-      //console.log(template_html);
-    }).then(function () {
-      // It's all done, hide the spinner ;)
-      //$('.testing-spinner').hide();
-    });
-  }, Promise.resolve());
+  Promise.resolve().then(function() {
+    return exploits.reduce(function (sequence, exploit) {
+      return sequence.then(function () {
+        //console.log(exploit);
+        return post(exploitableCheckURL, {websiteUrl: exploit.url, header: exploit.header});
+      }).then(function(result) {
+        //console.log(result);
+        // Generate the HTML of the template
+        template_html = alertTemplate({exploitable: result.response.exploitable, url: result.dat.websiteUrl, header: result.dat.header});
+        // Append it to the results well
+        $('.results-well').append(template_html).fadeIn();
+        //console.log(template_html);
+      });
+    }, Promise.resolve());
+  }).catch(function(err) {
+    // Catch any error that comes our way
+    alert('It\'s bloody broken! ' + err.message);
+    //console.log(err);
+  }).then(function() {
+    // It's all done, hide the spinner ;)
+    //console.log('all done!');
+    $('.testing-spinner').hide();
+  });
 });
