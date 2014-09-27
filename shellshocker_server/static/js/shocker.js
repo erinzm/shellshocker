8dc23a08333ffc27de4c99589064a14ccc32fca3
Liam M <archimedespi3141@gmail.com>
2014-09-26 22:56:34 -0500

    Fix. Deploying to Heroku

--- shellshocker_server/static/js/shocker.js
+++ shellshocker_server/static/js/shocker.js
@@ -6,20 +6,26 @@ $(document).ready(function() {
     url = urlsToCheck[i]
     for (var j = 0; j < headersToCheck.length; j++) {
       header = headersToCheck[j]
-      console.log('Checking URL ' + url + ' with header ' + header)
-      $.post(exploitableCheckURL, {
-        websiteUrl: url,
-        header: header
-      }).done(function (response) {
-        exploitable = response['exploitable'];
-        console.log("This URL is exploitable? " + exploitable);
-        template_html = alertTemplate({exploitable: exploitable, url: url, header: header});
-        $('.results-well').append(template_html).fadeIn();
-        console.log(template_html);
-        $('.testing-spinner').remove()
-      }).fail(function () {
-        console.log("Server request failed");
+      var response = "";
+      console.log('Checking URL ' + url + ' with header ' + header);
+      $.ajax({
+        type: 'POST',
+        async: false,
+        url: exploitableCheckURL,
+        data: {
+          websiteUrl: url,
+          header: header
+        },
+        success: function(result) {
+          response = result;
+        }
       });
+      exploitable = response['exploitable'];
+      console.log("This URL is exploitable? " + exploitable);
+      template_html = alertTemplate({exploitable: exploitable, url: url, header: header});
+      $('.results-well').append(template_html).fadeIn();
+      console.log(template_html);
+      $('.testing-spinner').remove()
     }
   }
 });
